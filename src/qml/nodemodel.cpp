d04a13eb2a86393c00c31fbd659689f692b1385d
shaavan <shaavan.github@gmail.com>
2022-09-27 20:43:36 +0530

    qml: Implements functions declared in nodemodel.h

--- src/qml/nodemodel.cpp
+++ src/qml/nodemodel.cpp
@@ -10,6 +10,7 @@
 #include <cassert>
 #include <chrono>
 
+#include <QDateTime>
 #include <QTimerEvent>
 
 NodeModel::NodeModel(interfaces::Node& node)
@@ -28,9 +29,84 @@ void NodeModel::setBlockTipHeight(int new_height)
 
 void NodeModel::setVerificationProgress(double new_progress)
 {
+    QDateTime currentDate = QDateTime::currentDateTime();
+
+    // keep a vector of samples of verification progress at height
+    m_block_process_time.push_front(qMakePair(int(currentDate.toMSecsSinceEpoch()), new_progress));
+
+    // show progress speed if we have more than one sample
+    if (m_block_process_time.size() >= 2) {
+        double progressDelta = 0; //
+        int timeDelta = 0; //
+        int remainingMSecs = 0;
+        double remainingProgress = 1.0 - new_progress; //
+        for (int i = 1; i < m_block_process_time.size(); i++) {
+            QPair<int, double> sample = m_block_process_time[i];
+
+            // take first sample after 500 seconds or last available one
+            if (sample.first < (currentDate.toMSecsSinceEpoch() - 500 * 1000) || i == m_block_process_time.size() - 1) {
+                progressDelta = m_block_process_time[0].second - sample.second;
+                timeDelta = m_block_process_time[0].first - sample.first;
+                remainingMSecs = (progressDelta > 0) ? remainingProgress / progressDelta * timeDelta : -1;
+                break;
+            }
+        }
+        if(remainingMSecs > 0 && m_block_process_time.count() % 1000 == 0) {
+            m_remaining_time = remainingMSecs;
+
+            Q_EMIT remainingTimeChanged();
+        }
+        static const int MAX_SAMPLES = 5000;
+        if (m_block_process_time.count() > MAX_SAMPLES) {
+            m_block_process_time.remove(1, m_block_process_time.count() - 1);
+        }
+    }
+
     if (new_progress != m_verification_progress) {
         m_verification_progress = new_progress;
         Q_EMIT verificationProgressChanged();
+    }
+}
+
+bool NodeModel::setBlockTimeList(int new_block_time)
+{
+    int currentTime = QDateTime::currentDateTime().toSecsSinceEpoch();
+    int sec_in_12_hours = 12 * 60 * 60;
+    int time_at_12th_hour = currentTime - currentTime % sec_in_12_hours;
+
+    if (new_block_time < time_at_12th_hour) {
+        return false;
+    }
+
+    double ratio_of_12_hour_passed = (new_block_time - time_at_12th_hour) / (double)sec_in_12_hours;
+
+    m_block_time_list.push_back(ratio_of_12_hour_passed);
+    Q_EMIT blockTimeListChanged();
+    return true;
+}
+
+void NodeModel::setBlockTimeListInitial(const CBlockIndex* pblockindex)
+{
+    if (setBlockTimeList(pblockindex->nTime)) {
+        setBlockTimeListInitial(pblockindex->pprev);
+        return;
+    }
+    std::reverse(m_block_time_list.begin()+1, m_block_time_list.end());
+    Q_EMIT blockTimeListChanged();
+}
+
+void NodeModel::setCurrentTimeRatio()
+{
+    int currentTime = QDateTime::currentDateTime().toSecsSinceEpoch();
+    int sec_in_12_hours = 12 * 60 * 60;
+
+    double ratio_of_time_passed_since_12th_hour = (currentTime % sec_in_12_hours) / double(sec_in_12_hours);
+    if(m_current_time_ratio != ratio_of_time_passed_since_12th_hour) {
+        if (m_current_time_ratio > ratio_of_time_passed_since_12th_hour) {
+            m_block_time_list.clear();
+        }
+        m_current_time_ratio = ratio_of_time_passed_since_12th_hour;
+        Q_EMIT currentTimeRatioChanged();
     }
 }
 
@@ -44,6 +120,13 @@ void NodeModel::initializeResult([[maybe_unused]] bool success, interfaces::Bloc
     // TODO: Handle the `success` parameter,
     setBlockTipHeight(tip_info.block_height);
     setVerificationProgress(tip_info.verification_progress);
+
+    const CBlockIndex* tip;
+    node::NodeContext* context = m_node.context();
+    ChainstateManager* chainman = context->chainman.get();
+    tip = chainman->ActiveChain().Tip();
+
+    setBlockTimeListInitial(tip);
 }
 
 void NodeModel::startShutdownPolling()
@@ -73,5 +156,6 @@ void NodeModel::ConnectToBlockTipSignal()
         [this](SynchronizationState state, interfaces::BlockTip tip, double verification_progress) {
             setBlockTipHeight(tip.block_height);
             setVerificationProgress(verification_progress);
+            setBlockTimeList(tip.block_time);
         });
 }
