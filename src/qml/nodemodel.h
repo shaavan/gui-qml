4263629620bad879654a4e8dbb6dbed15dfeb8c1
shaavan <shaavan.github@gmail.com>
2022-09-27 20:38:48 +0530

    qml: Introduce new function and member variables needed to run the BlockClock
    Brief description of introduced functions:
    
    - m_remaining_time -> tracks the time remaining for syncing of
      Blockchain
    - m_block_time_list -> Keeps track of the time of the block mined after
      the last 12th hour.
    - m_current_time_ratio -> Keeps track of the percentage of 12 hours
      passed since last 12th hour.

--- src/qml/nodemodel.h
+++ src/qml/nodemodel.h
@@ -5,12 +5,16 @@
 #ifndef BITCOIN_QML_NODEMODEL_H
 #define BITCOIN_QML_NODEMODEL_H
 
+#include <chain.h>
+
 #include <interfaces/handler.h>
 #include <interfaces/node.h>
+#include <node/context.h>
 
 #include <memory>
 
 #include <QObject>
+#include <QVariant>
 
 QT_BEGIN_NAMESPACE
 class QTimerEvent;
@@ -25,15 +29,24 @@ class NodeModel : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(int blockTipHeight READ blockTipHeight NOTIFY blockTipHeightChanged)
+    Q_PROPERTY(int remainingSyncTime READ remainingSyncTime NOTIFY remainingTimeChanged)
     Q_PROPERTY(double verificationProgress READ verificationProgress NOTIFY verificationProgressChanged)
+    Q_PROPERTY(QVariantList blockTimeList READ blockTimeList NOTIFY blockTimeListChanged)
+    Q_PROPERTY(double currentTimeRatio READ currentTimeRatio NOTIFY currentTimeRatioChanged)
 
 public:
     explicit NodeModel(interfaces::Node& node);
 
     int blockTipHeight() const { return m_block_tip_height; }
     void setBlockTipHeight(int new_height);
+    int remainingSyncTime() const { return m_remaining_time; }
     double verificationProgress() const { return m_verification_progress; }
     void setVerificationProgress(double new_progress);
+    QVariantList blockTimeList() const { return m_block_time_list; }
+    bool setBlockTimeList(int new_block_time);
+    void setBlockTimeListInitial(const CBlockIndex* pblockindex);
+    double currentTimeRatio() const { return m_current_time_ratio; }
+    void setCurrentTimeRatio();
 
     Q_INVOKABLE void startNodeInitializionThread();
 
@@ -45,9 +58,12 @@ public Q_SLOTS:
 
 Q_SIGNALS:
     void blockTipHeightChanged();
+    void remainingTimeChanged();
     void requestedInitialize();
     void requestedShutdown();
     void verificationProgressChanged();
+    void blockTimeListChanged();
+    void currentTimeRatioChanged();
 
 protected:
     void timerEvent(QTimerEvent* event) override;
@@ -55,9 +71,14 @@ protected:
 private:
     // Properties that are exposed to QML.
     int m_block_tip_height{0};
+    int m_remaining_time{0};
     double m_verification_progress{0.0};
+    QVariantList m_block_time_list{0};
+    double m_current_time_ratio{0.0};
 
     int m_shutdown_polling_timer_id{0};
+
+    QVector<QPair<int, double>> m_block_process_time;
 
     interfaces::Node& m_node;
     std::unique_ptr<interfaces::Handler> m_handler_notify_block_tip;
